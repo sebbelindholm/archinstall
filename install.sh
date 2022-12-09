312b67275b4752524ea5d87e003464c2b4221273
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2022-12-09 15:39:23 +0100

    Updated for new setup.

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ EOF
 mkfs.fat -F 32 /dev/nvme1n1p1
 mount /dev/nvme1n1p2 /mnt
 mount --mkdir /dev/nvme1n1p1 /mnt/boot
-pacstrap /mnt base base-devel linux linux-firmware dunst networkmanager polybar gtk-engine-murrine kitty nautilus vim man-db man-pages zsh-syntax-highlighting zsh-completions texinfo intel-ucode grub efibootmgr xorg-server i3-gaps rofi dmenu firefox alacritty ranger lightdm lightdm-gtk-greeter nvidia i3status neofetch git nvidia-settings discord pulseaudio pavucontrol nitrogen picom os-prober deepin-screenshot zsh btop gtk3 lxappearance ttf-iosevka-nerd zip unzip
+pacstrap /mnt linux base base-devel linux-firmware archlinux-keyring grub efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
 echo "root:$1" >> /mnt/pass.txt
 echo "sebastian:$2" >> /mnt/pass.txt
@@ -45,32 +45,14 @@ END
 cat >> /etc/hostname << "END"
 arch-desktop
 END
-ln -sf /home/sebastian/github/dotfiles/grub/sleek /usr/share/grub/themes
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-cat >> /etc/default/grub << "END"
-GRUB_DISABLE_OS_PROBER=false
-END
-cat >> /etc/default/grub << "END"
-GRUB_THEME="/usr/share/grub/themes/sleek/theme.txt"
-END
-mount --mkdir /dev/nvme0n1p1 /mnt/windowsefi
 grub-mkconfig -o /boot/grub/grub.cfg
-systemctl enable NetworkManager
-systemctl enable lightdm
+systemctl enable Networkmanager
+systemctl enable sddm
 useradd -g wheel -m sebastian
 chpasswd < pass.txt
 rm -rf pass.txt
 echo "%wheel ALL=(ALL:ALL) NOPASSWD :ALL" >> /etc/sudoers
-ln -sf /home/sebastian/github/dotfiles/.config /home/sebastian/
-ln -sf /home/sebastian/github/dotfiles/X11/00-keyboard.conf /etc/X11/xorg.conf.d/
-ln -sf /home/sebastian/github/dotfiles/Xresources/.Xresources /home/sebastian/
-ln -sf /home/sebastian/github/dotfiles/vim/.vimrc /home/sebastian/
-ln -sf /home/sebastian/github/dotfiles/X11/xorg.conf /etc/X11/
-ln -sf /home/sebastian/github/dotfiles/X11/00-keyboard.conf /etc/X11/xorg.conf.d/
-ln -sf /home/sebastian/github/dotfiles/fonts /home/sebastian/.local/share/
-ln -sf /home/sebastian/github/dotfiles/zsh/.zshrc /home/sebastian/
-ln -sf /home/sebastian/github/dotfiles/.themes /home/sebastian/
-ln -sf /home/sebastian/github/dotfiles/icons/.icons /home/sebastian/
 su sebastian
 cd /home/sebastian
 git clone https://aur.archlinux.org/yay.git
@@ -78,14 +60,17 @@ cd yay
 makepkg -si --noconfirm
 cd ..
 rm -rf yay
-yay -S --noconfirm google-chrome nerd-fonts-complete rofi-wifi-menu-git
+yay -S --noconfirm google-chrome nerd-fonts-complete rofi-wifi-menu-git shell-color-scripts spotify minecraft-launcher
 mkdir github
-mkdir Download
+mkdir Downloads
 mkdir Pictures
 mkdir Documents
-cd /home/sebastian/github
+cd github
+git clone https://github.com/sebbelindholm/arch-install-script.git
+pacman -S - < packages.txt
+cd /home/sebastian
 git clone https://www.github.com/sebbelindholm/dotfiles.git
-git clone https://www.gitlab.com/dwt1/wallpapers.git
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 EOF
 sed -n '$d' /mnt/etc/sudoers
 sed -n '$d' /mnt/etc/sudoers
