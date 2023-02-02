eb5fbcf5909518a29f2d995d6878e90095d05397
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2023-02-02 21:27:57 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
 umount -R /mnt
 timedatectl set-ntp true
-fdisk /dev/nvme1n1 << "EOF"
-g
-n
 
+wipefs --all /dev/nvme1n1
+sgdisk /dev/nvme1n1 -o
+sgdisk /dev/nvme1n1 -n 1::+512MiB -t 1:ef00
+sgdisk /dev/nvme1n1 -n 2
+mkfs.vfat -F32 /dev/nvme1n1p1
+mkfs.ext4  -q /dev/nvme1n1p2
 
-+550M
-n
-
-
-
-t
-1
-1
-t
-2
-23
-w
-EOF
-mkfs.ext4 /dev/nvme1n1p2 << "EOF"
-y
-EOF
-mkfs.fat -F 32 /dev/nvme1n1p1
 mount /dev/nvme1n1p2 /mnt
 mount --mkdir /dev/nvme1n1p1 /mnt/boot
+mount --mkdir /dev/nvme0n1p1 /mnt/nvme
+mount --mkdir /dev/sda1 /mnt/hdd
+
 rm -rf /etc/pacman.conf
 cp pacman.conf /etc/
-pacstrap /mnt linux base base-devel linux-firmware archlinux-keyring grub efibootmgr git
+pacstrap /mnt linux base base-devel linux-firmware archlinux-keyring xdg-user-dirs
 rm -rf /mnt/etc/pacman.conf
 cp pacman.conf /mnt/etc/pacman.conf
-cp xorg.conf /mnt/etc/X11/
-mount --mkdir /dev/nvme0n1p1 /mnt/mnt/nvme1TB
-mount --mkdir /dev/sda1 /mnt/mnt/3TB
+
 genfstab -U /mnt >> /mnt/etc/fstab
+
 echo "root:$1" >> /mnt/pass.txt
 echo "sebastian:$2" >> /mnt/pass.txt
-cp git-checkout.sh /mnt
+
 arch-chroot /mnt /bin/bash << "EOF"
 ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 hwclock --systohc
@@ -53,8 +41,6 @@ END
 cat >> /etc/hostname << "END"
 arch-desktop
 END
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
 useradd -g wheel -m sebastian
 chpasswd < pass.txt
 rm -rf pass.txt
@@ -66,29 +52,18 @@ cd yay
 makepkg -si --noconfirm
 cd ..
 rm -rf yay
-yay --noconfirm -S nerd-fonts-complete google-chrome shell-color-scripts
-mkdir github
-mkdir github/cloned-repos
-mkdir github/personal-repos
-mkdir Downloads
-mkdir Pictures
-mkdir Documents
-cd github
+xdg-user-dirs-update
+mkdir Github
+mkdir Github/Personal
+mkdir Github/Cloned
+cd Github/Personal
 git clone https://github.com/sebbelindholm/arch-install-script.git
 cd arch-install-script
 sudo pacman --noconfirm -Syu - < pacman_packages.txt
-sudo git clone https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
-sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
 cd /home/sebastian
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "[Theme]
-Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
-/git-checkout.sh
-sudo rm /git-checkout.sh
 sudo systemctl enable NetworkManager
-sudo systemctl enable sddm
 sudo systemctl --user enable emacs
-sudo chsh -s /usr/bin/zsh
 EOF
 
 sed -n '$d' /mnt/etc/sudoers
